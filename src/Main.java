2e90da14c71e8f9148a96d24db74743038a8fe8a
Adrian Javier <javier.adrian.kld@gmail.com>
2024-05-02 00:48:19 +0800

    set FlatLaF as the theme

--- src/Main.java
+++ src/Main.java
@@ -61,8 +61,8 @@ public class Main extends javax.swing.JFrame {
         private void initComponents() {
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-                setPreferredSize(new java.awt.Dimension(640, 640));
-                setSize(new java.awt.Dimension(640, 640));
+                setPreferredSize(new java.awt.Dimension(720, 576));
+                setSize(new java.awt.Dimension(720, 576));
                 getContentPane().setLayout(new java.awt.CardLayout());
 
                 pack();
@@ -72,30 +72,7 @@ public class Main extends javax.swing.JFrame {
 	 * @param args the command line arguments
 	 */
 	public static void main(String args[]) {
-		/* Set the Nimbus look and feel */
-		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-		 */
-//		try {
-//			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//				if ("Nimbus".equals(info.getName())) {
-//					javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//					javax.swing.UIManager.setLookAndFeel(new FlatLightLaf());
-//					break;
-//				}
-//			}
-//		} catch (ClassNotFoundException ex) {
-//			java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (InstantiationException ex) {
-//			java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (IllegalAccessException ex) {
-//			java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//			java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		}
-		//</editor-fold>
-
+		/* Set look and feel */
 		try {
 			UIManager.setLookAndFeel(new FlatLightLaf());
 		} catch (Exception e) {
